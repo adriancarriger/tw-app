6e6095c61b855f1698e4849b903cf133c280082e
Adrian Carriger <hello@adriancarriger.com>
2018-07-28 15:56:16 -0500

    feat(deploy): auto deploy to netlify

---
+++ .circleci/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e
+
+# Run from parent directory
+cd "$( dirname "$0" )" && cd ..
+
+if [[ ! -d ~/.netlify ]]; then
+  mkdir ~/.netlify
+fi
+
+echo "{\"access_token\":\"$NETLIFY_CLI_TOKEN\"}" > ~/.netlify/config
+
+if [[ $CIRCLE_BRANCH == "develop" ]]; then
+  VUE_APP_SERVER_URI=$SERVER_URI_STAGING yarn run build
+  yarn run deploy
+fi
+
+if [[ $CIRCLE_BRANCH == "master" ]]; then
+  VUE_APP_SERVER_URI=$SERVER_URI_PRODUCTION yarn run build
+  yarn run deploy -e production
+fi
