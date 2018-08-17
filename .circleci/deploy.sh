0597777aa03c32e6cca6e7f514f05c2dfd41fe26
Adrian Carriger <hello@adriancarriger.com>
2018-08-17 06:53:08 -0500

    feat(auth): add auth0 authentication

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -12,11 +12,15 @@ fi
 echo "{\"access_token\":\"$NETLIFY_CLI_TOKEN\"}" > ~/.netlify/config
 
 if [[ $CIRCLE_BRANCH == "develop" ]]; then
-  VUE_APP_GRAPHQL_HTTP=$SERVER_URI_STAGING yarn run build
+  VUE_APP_SERVER_URI=$SERVER_URI_STAGING \
+    VUE_APP_URI=$APP_URI_STAGING \
+    yarn run build
   yarn run deploy
 fi
 
 if [[ $CIRCLE_BRANCH == "master" ]]; then
-  VUE_APP_GRAPHQL_HTTP=$SERVER_URI_PRODUCTION yarn run build
+  VUE_APP_SERVER_URI=$SERVER_URI_PRODUCTION \
+    VUE_APP_URI=$APP_URI_PRODUCTION \
+    yarn run build
   yarn run deploy -e production
 fi
